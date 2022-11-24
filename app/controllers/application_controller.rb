3877f22a89230417af376f1b27b48a00831cfca4
collin chua <collin-chua@hotmail.com>
2022-11-24 14:33:47 +0800

    Added a funcction to disable dropdown without logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   private
-
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :user
