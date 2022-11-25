c152857f36cb64873382c5560d5e5faf5c549443
collin chua <collin-chua@hotmail.com>
2022-11-25 16:01:44 +0800

    added sidebar, finally

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sidebar"
 export default class extends Controller {
+  static targets = ["button"]
+
   connect() {
+    console.log("Hello")
   }
 }
