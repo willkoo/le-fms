d246637341ddfac592a864a40877d0b3743fc1ac
Daryl Chong <darylchonggz@gmail.com>
2022-11-25 17:52:43 +0800

    final migrations solved

--- db/migrate/20221124073421_remove_column_from_company_profiles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromCompanyProfiles < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :company_profiles, :verified
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_162753) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_071143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,9 +36,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_162753) do
     t.string "uen", null: false
     t.text "address", null: false
     t.bigint "profile_id", null: false
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.string "url"
     t.string "paid_up_capital", null: false
     t.string "last_fy_revenue", null: false
