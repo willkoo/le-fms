fc0c3f47e2ea699abcb1f52a7073023c5fdd6414
Lester Wee <lester.wee.68@gmail.com>
2022-11-25 16:13:01 +0800

    seed file works, able to connect 10 models together.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ puts "cleaning database"
 User.destroy_all
 Franchise.destroy_all
 Profile.destroy_all
-Company_Profile.destroy_all
+CompanyProfile.destroy_all
 Licence.destroy_all
 Partner.destroy_all
 Quiz.destroy_all
@@ -25,16 +25,16 @@ User.create(email: "user1@gmail.com", password: "password")
 User.create(email: "user2@gmail.com", password: "password")
 
 puts "Creating franchises..."
-Franchise.create(name: "Starbucks", user_id: "1", description: "It happens millions of times each week – a customer receives a drink from a Starbucks barista - but each interaction is unique.", url: "www.starbucks.com.sg")
+Franchise.create(name: "Starbucks", user_id: "1", description: "It happens millions of times each week – a customer receives a drink from a Starbucks barista - but each interaction is unique.")
 
 puts "Creating (user) profiles..."
 Profile.create(user_id: "2", first_name: "Collin", last_name: "Chua", contact_number: "91234567", description: "This is a tester account 1 with a company assigned", country: "Singapore")
 Profile.create(user_id: "3", first_name: "Willie", last_name: "Khoo", contact_number: "98892834", description: "This is another tester account 2 with 2 companies assigned", country: "Singapore")
 
 puts "Creating companies..."
-CompanyProfile.create(name: "1north", uen: "200112345A", address: "1north towers", profile_id: "2", url: "www.1north.com", status: "verified")
-CompanyProfile.create(name: "2sky", uen: "200122325B", address: "2sky towers", profile_id: "2", url: "www.2ky.com", status: "rejected")
-CompanyProfile.create(name: "2net", uen: "200122325B", address: "2net towers", profile_id: "3", url: "www.2net.com", status: "pending")
+CompanyProfile.create(name: "1north", uen: "200112345A", address: "1north towers", profile_id: "2", url: "www.1north.com", status: "accepted", paid_up_capital: 100000, last_fy_revenue: 800000, legal_disputes: "No legal issues.")
+CompanyProfile.create(name: "2sky", uen: "200122325B", address: "2sky towers", profile_id: "2", url: "www.2ky.com", status: "rejected", paid_up_capital: 200000, last_fy_revenue: 700000, legal_disputes: "No legal issues.")
+CompanyProfile.create(name: "2net", uen: "200122325B", address: "2net towers", profile_id: "3", url: "www.2net.com", status: "pending", paid_up_capital: 300000, last_fy_revenue: 600000, legal_disputes: "No legal issues.")
 
 puts "Creating licenses..."
 Licence.create(company_profile_id: "1", franchise_id: "1", proposed_location: "20 Collyer Quay", licence_status: "approved")
