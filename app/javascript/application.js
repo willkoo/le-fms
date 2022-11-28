b7b5c06d4da42241f9e6e27a3ef3e7dbc915dbb8
Willie Koo <williekoo@hotmail.com>
2022-11-28 12:15:17 +0800

    index minor change

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,12 @@
 // Entry point for the build script in your package.json
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery
+//= require popper
+//= require bootstrap
+//= require datatables
+//= require_tree .
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
