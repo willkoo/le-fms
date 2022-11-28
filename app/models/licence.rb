6ba88f803c47e913a7eae73df009b133109d5839
Willie Koo <williekoo@hotmail.com>
2022-11-28 16:47:19 +0800

    fixed upload issue in licence form

--- app/models/licence.rb
+++ app/models/licence.rb
@@ -3,7 +3,7 @@ class Licence < ApplicationRecord
   belongs_to :franchise
   has_many :license_comments
   has_many :partners
-  has_one_attached :pdf
+  has_one_attached :proposal
 
   validates :company_profile_id, :franchise_id, :proposed_location, :licence_status, presence: true
   validates :licence_status, inclusion: { in: %w[approve reject pending] }
