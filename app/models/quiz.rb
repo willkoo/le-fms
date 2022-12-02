29c79cf49cd94ee00f6d14524249948ea1236756
Daryl Chong <darylchonggz@gmail.com>
2022-12-02 11:09:31 +0800

    add effects to app pages

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,4 +4,8 @@ class Quiz < ApplicationRecord
   has_many :quiz_attempts, dependent: :destroy
 
   # validates :quiz_name, :franchise_id, :video_url, :video_completed, presence: true
+
+  def passed?
+    quiz_attempts.pluck(:status).include?("pass")
+  end
 end
