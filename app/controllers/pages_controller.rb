74b0607c68bfb19ac45d25b8e47fdf9336fa30db
Willie Koo <williekoo@hotmail.com>
2022-12-02 11:11:19 +0800

    Revert "test zonghan"
    This reverts commit 9635ec8b6681701affe6b2c1aa632c36c4a6adaf.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, except: [:hello_sign]
-  skip_between_action :verify_authenticity_token
-
   def hello_sign
     render json: {
       success: true
