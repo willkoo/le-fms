9635ec8b6681701affe6b2c1aa632c36c4a6adaf
Willie Koo <williekoo@hotmail.com>
2022-12-02 11:07:23 +0800

    test zonghan

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, except: [:hello_sign]
+  skip_between_action :verify_authenticity_token
+
   def hello_sign
     render json: {
       success: true
